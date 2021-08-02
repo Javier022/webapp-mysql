8d756189a35293d55fe9c8a032fef2d2d9519aa3
javier <hrzjavier09@gmail.com>
2021-08-02 13:01:37 -0600

    test to main component <App/>

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,28 @@
+import { create } from "react-test-renderer";
+import App from "../App";
+
+describe("<App />", () => {
+  let component;
+  let provider;
+
+  beforeAll(() => {
+    component = create(<App />);
+    provider = component.root.type("DataProvider");
+  });
+
+  it("render correctly", () => {
+    expect(component).toBeDefined();
+  });
+
+  it("has provider", () => {
+    expect(provider).toBeDefined();
+  });
+
+  it("contains routes and toast", () => {
+    const routes = provider.props.children[0].type;
+    const toastContainer = provider.props.children[1].type;
+
+    expect(routes).toBeDefined();
+    expect(toastContainer).toBeDefined();
+  });
+});
